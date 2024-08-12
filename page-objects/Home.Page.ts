7442ddfd3823b0f2b8e0c2ee305206d0cdaaae34
carlnoval <cjrnoval22@gmail.com>
2024-08-12 19:47:46 +0900

    Add: First item locator

--- page-objects/Home.Page.ts
+++ page-objects/Home.Page.ts
@@ -8,9 +8,11 @@ import { Locator, Page } from "@playwright/test";
 export class HomePage {
   readonly page: Page;
   readonly homePageHeader: Locator;
+  readonly homePageFirstItem: Locator;
 
   constructor(page: Page) {
     this.page  = page;
     this.homePageHeader = page.getByText('Swag Labs');
+    this.homePageFirstItem = page.locator('[data-test="inventory-list"] div').filter({ hasText: 'Sauce Labs Backpackcarry.' }).first();
   }
 }
